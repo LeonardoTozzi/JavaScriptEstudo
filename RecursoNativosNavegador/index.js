d7c8ceb124523b6be328aa7eca4bef0ebb95fcee
Leonardo Tozzi <leonardotozzis@gmail.com>
2022-12-16 16:45:12 -0300

    Em andamento

---
+++ RecursoNativosNavegador/index.js
@@ -0,0 +1,18 @@
+let nome = "Leonardo"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert (nome)
+// nome = window.prompt ("Insira o seu nome: ")
+// let confirmacao = window.confirm("Confirma essa ação? ")
+
+// console.log(confirmacao)
+
+
+//comandos que não precisa necessariamente usar o WINDOW
+//                      
+alert("Olá mundo!")
+prompt("Olá")
+confirm("Confirma?")
