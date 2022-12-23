637dd680a73858da930a36767da3f34267bff32b
Leonardo Tozzi <leonardotozzis@gmail.com>
2022-12-23 15:08:00 -0300

    Finalizado

---
+++ ExerciciosJS/viagem/index.js
@@ -0,0 +1,49 @@
+const nomeTurista = prompt("Qual o seu nome?")
+let cidade = ""
+contagem = 0
+
+let confirmar = (nomeTurista + " você já visitou alguma cidade? (Sim/Não")
+
+while (confirmar === "Sim") {
+    let cidades = prompt("Qual cidade você visitou?")
+    cidades += " - "
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Código que tentei fazer sozinho:
+
+const nome = prompt("Qual o seu nome?")
+let visita = prompt(nome + " já vistiou alguma cidade? (Sim/Não) ")
+
+cidade = 0
+
+while (visita === "Sim") {
+    let cidade = prompt("Qual o nome da cidade?")
+    cidade += 1
+    
+    if (visita === "Não"){
+        break
+        
+    }
+}
+
+alert("Visitou;" +
+"\n" + cidade
+)*/
