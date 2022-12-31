c1a25e951835a81e64884880d8d7b87316409045
Leonardo Tozzi <leonardotozzis@gmail.com>
2022-12-30 21:42:49 -0300

    Ainda estudando

---
+++ ExerciciosJS/consultorio/index.js
@@ -0,0 +1,25 @@
+let fila = []
+let opcao = ""
+
+do {
+    let pacientes = ""
+    for (let i = 0; i < fila.length; i++) {
+        pacientes += (i + 1) + "º - " + fila[i] + "\n"
+    }
+
+    opcao = prompt(
+        "pacientes:\n" + pacientes +
+        "\nEscolha uma ação:\n1. Novo paciente\n2. Consultar pacientes\n3. Sair"
+    )
+
+    switch (opcao){
+        case "1":
+            const novoPaciente = prompt("Qual é o nome do paciente?")
+            fila.push(novoPaciente)
+            break
+        case "2":
+            const pacienteConsultado = fila.shift()
+            alert(pacienteConsultado + " ")
+            break
+    }
+} while (opcao !== "3")
