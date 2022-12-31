2e7832917334393d1c010162b481bbd28071d732
Leonardo Tozzi <leonardotozzis@gmail.com>
2022-12-31 11:59:51 -0300

    Finalizado

--- ExerciciosJS/consultorio/index.js
+++ ExerciciosJS/consultorio/index.js
@@ -8,18 +8,28 @@ do {
     }
 
     opcao = prompt(
-        "pacientes:\n" + pacientes +
+        "Pacientes:\n" + pacientes +
         "\nEscolha uma ação:\n1. Novo paciente\n2. Consultar pacientes\n3. Sair"
     )
 
-    switch (opcao){
+    switch (opcao) {
         case "1":
             const novoPaciente = prompt("Qual é o nome do paciente?")
             fila.push(novoPaciente)
             break
         case "2":
             const pacienteConsultado = fila.shift()
-            alert(pacienteConsultado + " ")
+            if (pacienteConsultado){
+                alert(pacienteConsultado + " foi removido da fila.")
+            } else {
+                alert("Não há pacientes na fila!")
+            }
+          
             break
+        case "3":
+            alert("Encerrando...")
+            break
+        default:
+            alert("Opção inválida")
     }
 } while (opcao !== "3")
