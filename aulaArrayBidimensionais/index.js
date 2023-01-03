61aa515231241c14b68241c25802ede111aa62dc
Leonardo Tozzi <leonardotozzis@gmail.com>
2023-01-03 09:49:13 -0300

    finalizado

---
+++ aulaArrayBidimensionais/index.js
@@ -0,0 +1,48 @@
+const arr = [
+    "1º Nível",
+    ["2º nivel", 42, true], 
+    [
+        ["3º nível, 1º item", "Olá, mundo!"],
+        ["3º nível, 2º item", "Oi, mundo!"],
+    ],
+    []
+]
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[1][1])
+console.log(arr[2])
+console.log(arr[2][1])
+console.log(arr[2][1][0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const arr = [
+    [
+        "1º Nível",
+        "2º Nível",
+        "3º Nível",
+    ],[
+        "1º Nível",
+        "2º Nível",
+        "3º Nível",
+    ],[
+        "1º Nível",
+        "2º Nível",
+        "3º Nível",
+    ]
+  ]
+  console.log(arr)*/ 
